642d28011b945655b77dabc3774fc20104ea6116
brett <waymond91@gmail.com>
2021-02-24 13:37:10 +0400

    Fixed component/sandbox

--- src/components/sandbox.js
+++ src/components/sandbox.js
@@ -7,10 +7,10 @@ import Embed from "./embed";
 
 export default function Sandbox (){
     const codeSrc = "https://codepen.io/waymond91/embed/yLVyYOq?height=308&theme-id=light&default-tab=js,result"
-    
-    //const content = `I'm really not too attached to this demo, it doesn't really showcase the right tech`
+
+    const content = `I'm really not too attached to this demo, it doesn't really showcase the right tech`
     // I commented this out as the page is publicly accessible...
-    
+
     return(
         <div>
             <Embed
