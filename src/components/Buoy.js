1ca96deb26d28693ac13461c94eb6e143010eab0
Brett Smith <brettmsmith@protonmail.com>
2022-04-13 22:37:05 +0400

    SPGA Demo

--- src/components/Buoy.js
+++ src/components/Buoy.js
@@ -1,22 +1,136 @@
-import React, {useContext} from 'react'
+import React, { useContext, useEffect } from "react";
 import ResponsivePlot from "../components/ResponsivePlot";
-import {ProbeContext} from "../context/ProbeProvider";
-import {BackendContext} from "../context/BackendProvider";
+import { BackendContext } from "../context/BackendProvider";
 import Styles from "./Styles";
-import {Col} from 'react-bootstrap'
+import { Col, Row, Card, Button, ButtonGroup } from "react-bootstrap";
 
 export default function Buoy(props) {
-    //const {ProbeStatus, ProbeColor, ProbeVariant, ProbeDisabled} = useContext(ProbeContext)
-    const {Probes} = useContext(BackendContext)
+  const { Probes } = useContext(BackendContext);
 
+  function renderDateButtons() {
     return (
-        <Col style={Styles.BootstrapCenter}>
-            <ResponsivePlot data={Probes.do.data} width={0.5} height={.275} title={'DO'}
-                            isMobile={false} color={color}/>
-            <ResponsivePlot data={Probes.ph.data} width={0.5} height={.275} title={'pH'}
-                            isMobile={false} color={color}/>
-            <ResponsivePlot data={Probes.temp.data} width={0.5} height={.275} title={'Temp'}
-                            isMobile={false} color={color}/>
-        </Col>
-    )
+      <Row style={Styles.BootstrapCenter}>
+        <ButtonGroup
+          style={{ alignItems: "center", width: "50%", padding: "1em" }}
+        >
+          <Button
+            variant={"secondary"}
+            onClick={() => {
+              Probes.update();
+            }}
+          >
+            {"<"}
+          </Button>
+          <Button variant={"secondary"}>Date</Button>
+          <Button
+            variant={"secondary"}
+            onClick={() => {
+              Probes.update();
+            }}
+          >
+            {">"}
+          </Button>
+        </ButtonGroup>
+      </Row>
+    );
+  }
+
+  return (
+    <Card style={{ textAlign: "center", width: "90%" }}>
+      <Card.Body>
+        <Card.Title style={{ padding: "1em" }}>
+          <h5>{Probes.buoy}</h5>
+        </Card.Title>
+        <Card.Text>
+          <Row style={Styles.BootstrapCenter}>
+            <Col
+              lg={2}
+              style={{ transform: "rotate(-90deg)", fontSize: "0.75em" }}
+            >
+              DO (mg/L)
+            </Col>
+            <Col lg={10}>
+              <ResponsivePlot
+                data={Probes.do.data}
+                width={0.225}
+                height={0.15}
+                title={"DO"}
+                isMobile={false}
+              />
+            </Col>
+          </Row>
+          <Row style={Styles.BootstrapCenter}>
+            <Col
+              lg={2}
+              style={{ transform: "rotate(-90deg)", fontSize: "0.75em" }}
+            >
+              pH
+            </Col>
+            <Col lg={10}>
+              <ResponsivePlot
+                data={Probes.ph.data}
+                width={0.225}
+                height={0.15}
+                title={"pH"}
+                isMobile={false}
+              />
+            </Col>
+          </Row>
+          <Row style={Styles.BootstrapCenter}>
+            <Col
+              lg={2}
+              style={{
+                transform: "rotate(-90deg)",
+                fontSize: "0.75em"
+              }}
+            >
+              Temp (c)
+            </Col>
+            <Col lg={10}>
+              <ResponsivePlot
+                data={Probes.temp.data}
+                width={0.225}
+                height={0.15}
+                title={"Temp"}
+                isMobile={false}
+              />
+            </Col>
+          </Row>
+          <Row style={Styles.BootstrapCenter}>
+            <Col
+              lg={2}
+              style={{
+                transform: "rotate(-90deg)",
+                fontSize: "0.75em"
+              }}
+            >
+              Wave Height (m)
+            </Col>
+            <Col lg={10}>
+              <ResponsivePlot
+                data={Probes.wh.data}
+                width={0.225}
+                height={0.15}
+                title={"wh"}
+                isMobile={false}
+              />
+            </Col>
+          </Row>
+          {renderDateButtons()}
+          <Row style={Styles.BootstrapCenter}>
+            <Button style={{ margin: "0.5em", width: "50%" }}>Export</Button>
+            <Button
+              style={{ margin: "0.5em", width: "50%" }}
+              variant={"secondary"}
+              onClick={() => {
+                Probes.setVisible(false);
+              }}
+            >
+              Return
+            </Button>
+          </Row>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
--- src/context/BackendProvider.js
+++ src/context/BackendProvider.js
@@ -1,83 +1,93 @@
-import React, {createContext, useEffect, useReducer, useState} from "react";
+import React, { createContext, useEffect, useReducer, useState } from "react";
 
-export const BackendContext = createContext({})
+export const BackendContext = createContext({});
 
-export default function BackendProvider({children}) {
-    const probeTypes = {
-        DATA: "DATA",
-        READY: "READY",
-        MAX: "MAX",
-        MIN: "MIN",
-        OUT_OF_RANGE: "OUT_OF_RANGE"
+export default function BackendProvider({ children }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const [buoy, setBuoy] = useState("");
+  const probeTypes = {
+    DATA: "DATA",
+    READY: "READY",
+    MAX: "MAX",
+    MIN: "MIN",
+    OUT_OF_RANGE: "OUT_OF_RANGE"
+  };
+
+  const initialProbe = {
+    data: generateZeros(24),
+    ready: false,
+    max: null,
+    min: null,
+    outOfRange: false
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case probeTypes.DATA:
+        return { ...state, data: action.value };
+      case probeTypes.READY:
+        return { ...state, ready: action.value };
+      case probeTypes.MAX:
+        return { ...state, max: action.value };
+      case probeTypes.MIN:
+        return { ...state, min: action.value };
+      case probeTypes.OUT_OF_RANGE:
+        return { ...state, outOfRange: action.value };
     }
+  };
 
-    const initialProbe = {
-        data: generateZeros(24),
-        ready: false,
-        max: null,
-        min: null,
-        outOfRange: false
-    }
+  const [phState, phDispatch] = useReducer(reducer, initialProbe);
+  const [doState, doDispatch] = useReducer(reducer, initialProbe);
+  const [tpState, tpDispatch] = useReducer(reducer, initialProbe);
+  const [whState, whDispatch] = useReducer(reducer, initialProbe);
 
-    const reducer = (state, action) => {
-        switch (action.type) {
-            case probeTypes.DATA:
-                return {...state, data: action.value}
-            case probeTypes.READY:
-                return {...state, ready: action.value}
-            case probeTypes.MAX:
-                return {...state, max: action.value}
-            case probeTypes.MIN:
-                return {...state, min: action.value}
-            case probeTypes.OUT_OF_RANGE:
-                return {...state, outOfRange: action.value}
+  function newProbeData() {
+    phDispatch({ type: probeTypes.DATA, value: generateData(24, 8.1) });
+    phDispatch({ type: probeTypes.READY, value: true });
+    doDispatch({ type: probeTypes.DATA, value: generateData(24, 7.5) });
+    doDispatch({ type: probeTypes.READY, value: true });
+    tpDispatch({ type: probeTypes.DATA, value: generateData(24, 26) });
+    tpDispatch({ type: probeTypes.READY, value: true });
+    whDispatch({ type: probeTypes.DATA, value: generateData(24, 0.8) });
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      newProbeData();
+    }, 1000);
+  }, []);
+
+  function generateData(num, center) {
+    return [...new Array(num)].map((row, index) => ({
+      x: index,
+      y: Math.random() + center
+    }));
+  }
+
+  function generateZeros(num) {
+    return [...new Array(num)].map((row, index) => ({
+      x: index,
+      y: 0.0
+    }));
+  }
+
+  return (
+    <BackendContext.Provider
+      value={{
+        Probes: {
+          do: doState,
+          ph: phState,
+          temp: tpState,
+          wh: whState,
+          update: newProbeData,
+          visible: isVisible,
+          setVisible: setIsVisible,
+          buoy: buoy,
+          setBuoy: setBuoy
         }
-    }
-
-    const [phState, phDispatch] = useReducer(reducer,initialProbe);
-    const [doState, doDispatch] = useReducer(reducer,initialProbe);
-    const [tpState, tpDispatch] = useReducer(reducer,initialProbe);
-
-    function newProbeData(){
-        phDispatch({type: probeTypes.DATA, value: generateData(24,8.1)})
-        phDispatch({type: probeTypes.READY, value:true})
-        doDispatch({type: probeTypes.DATA, value: generateData(24, 7.5)})
-        doDispatch({type: probeTypes.READY, value:true})
-        tpDispatch({type: probeTypes.DATA, value: generateData(24, 26)})
-        tpDispatch({type: probeTypes.READY, value:true})
-    }
-
-    useEffect(() => {
-        setTimeout(() => {
-            newProbeData()
-        }, 1000)
-    }, [])
-
-    function generateData(num, center) {
-        return [...new Array(num)].map((row, index) => ({
-            x: index,
-            y: Math.random() + center
-        }));
-    }
-
-    function generateZeros(num) {
-        return [...new Array(num)].map((row, index) => ({
-            x: index,
-            y: 0.0
-        }));
-    }
-
-    return (
-        <BackendContext.Provider
-            value={{
-                Probes: {
-                    do: doState,
-                    ph: phState,
-                    temp: tpState,
-                    update:newProbeData,
-                }
-            }}>
-            {children}
-        </BackendContext.Provider>
-    )
+      }}
+    >
+      {children}
+    </BackendContext.Provider>
+  );
 }
