d92cdfba5fd252f343a9922c27e47b965bd6c06b
daniel-oceanlabs-seychelles <d.huegelmann@gmail.com>
2022-09-16 03:17:58 +0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -69,6 +69,24 @@ export default function Home() {
                     </Card>
                     <br/>
                 </Col>
+                <Col className="m-auto align-self-center" xs={12} sm={6} md={4} style={titleStyle}>
+                    <Card style={{background: "rgba(255,255,255,0.75)",}}>
+                        <Card.Title>
+                            <h1 style={{paddingTop:"2rem"}}>Engineering<b> Solutions</b></h1>
+                        </Card.Title>
+                        <Card.Img class="img-responsive center-block"  src={buoy}/>
+                        <Card.Body>
+                            <p style={{padding:"1rem"}}>We aim for the design and use of appropriate technology, suited to our climate, 
+                            infrastructure and the needs of our society and environment.</p>
+                            <Link to={'/buoylive'}>
+                                <Button style={{width:'100%'}}
+                                        className="btn btn-primary"> Buoy Demonstration</Button>
+                            </Link>
+                            <br/><br/>
+                        </Card.Body>
+                    </Card>
+                    <br/>
+                </Col>
             </Row>
         </div>
     );
