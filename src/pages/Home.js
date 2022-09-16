394e2c8f959f30df8cc8148f76e807a3c8e6095a
daniel-oceanlabs-seychelles <d.huegelmann@gmail.com>
2022-09-16 04:30:26 +0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -79,8 +79,8 @@ export default function Home() {
                         </Card.Title>
                         <Card.Img class="img-responsive center-block"  src={buoy}/>
                         <Card.Body>
-                            <p style={{padding:"1rem"}}>Our devices can collect data in even the most remote ocean and island locations and 
-                            safely transmit that data to your laptop or mobile phone via satellite or cellular data connections.
+                            <p style={{padding:"1rem"}}>Our devices can collect data in even the most remote locations and 
+                            safely transmit it to where you need it.
                             </p>
                             <Link to={'/buoylive'}>
                                 <Button style={{width:'100%'}}
